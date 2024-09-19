078c4a20847e5fb5eb508767d38f82bf76668988
João Flávio <joaoflaviog1@gmail.com>
2024-09-19 13:55:08 -0300

    185 salvando foto na db
    Colocado Foreign Key na tabela Foto do id_aluno e feito um relacionamento pertenceA (belongsTo) de Foto para Aluno

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -7,15 +7,15 @@ export default {
     if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
       return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG'));
     }
-
+    //         cb: callback
     return cb(null, true);
   },
   storage: multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: (req, file, cb) => { // define o destino (por isso importar o path.resolve)
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
     },
-    filename: (req, file, cb) => {
+    filename: (req, file, cb) => { // define o nome com o milissegundo atual
       cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
-    },
+    }, //       // aleatorio para nunca ter 2 iguais // extname: extensão do arq
   }),
 };
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 // primeiro importa os módulos (tecnologias), depois os internos (q eu criei)
 import fotoController from '../controllers/FotoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 //               upload: vai colocar 1 foto enviada pela web (imsomnia) para pasta uploads
-router.post('/', fotoController.store);
+router.post('/', loginRequired, fotoController.store);
 
 export default router;
